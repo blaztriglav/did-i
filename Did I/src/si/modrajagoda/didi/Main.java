50558cd94f0512ec5e2eaded56f776baa601dca1
blaz <blaz@modrajagoda.si>
2012-09-29 12:10:27 +0200

    Testing github hipchat integration

--- Did I/src/si/modrajagoda/didi/Main.java
+++ Did I/src/si/modrajagoda/didi/Main.java
@@ -14,6 +14,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Main extends FragmentActivity implements ActionBar.TabListener {
@@ -93,6 +94,7 @@ public class Main extends FragmentActivity implements ActionBar.TabListener {
             textView.setGravity(Gravity.CENTER);
             Bundle args = getArguments();
             textView.setText(Integer.toString(args.getInt(ARG_SECTION_NUMBER)));
+            Button button;
             return textView;
         }
     }
