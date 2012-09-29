c2eb3bae3b4c61ba9288a5dc4c462320a85aa4a6
Nejc Tomsic <nejc@modrajagoda.si>
2012-09-29 21:14:12 +0200

    Dynamically loading habits step one.

--- Did I/src/si/modrajagoda/didi/FragmentHabits.java
+++ Did I/src/si/modrajagoda/didi/FragmentHabits.java
@@ -1,5 +1,15 @@
 package si.modrajagoda.didi;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import si.modrajagoda.didi.database.DatabaseHelper;
+import si.modrajagoda.didi.database.Habit;
+
+import com.j256.ormlite.android.apptools.OpenHelperManager;
+import com.j256.ormlite.dao.Dao;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -19,11 +29,54 @@ public class FragmentHabits extends Fragment implements OnClickListener {
 	private int count = 1;
 	private int[] viewIndicators = {R.id.image_view_indicator_1, R.id.image_view_indicator_2, 
 			R.id.image_view_indicator_3, R.id.image_view_indicator_4, R.id.image_view_indicator_5};
+	
+	private ArrayList<String> habitQuestions = null;
+	private int questionCount = 0;
+	private DatabaseHelper databaseHelper = null;
+	private Dao<Habit, Integer> habitDao = null;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		view = inflater.inflate(R.layout.fragment_habits, container, false);
+		
+		// Get all the habits from the database
+		databaseHelper = getHelper();
+		habitQuestions = new ArrayList<String>();
+		try {
+			habitDao = databaseHelper.getHabitDao();
+			List<Habit> habits = habitDao.queryForAll();
+			questionCount = habits.size();
+			for(Habit habit : habits){
+				habitQuestions.add(habit.getName());
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		// Dynamically display number of habits indicator
+		ImageView indicator;
+		switch (questionCount) {
+		case 5:
+			indicator = (ImageView) view.findViewById(R.id.image_view_indicator_5);
+			indicator.setVisibility(View.VISIBLE);
+		case 4:
+			indicator = (ImageView) view.findViewById(R.id.image_view_indicator_4);
+			indicator.setVisibility(View.VISIBLE);
+		case 3:
+			indicator = (ImageView) view.findViewById(R.id.image_view_indicator_3);
+			indicator.setVisibility(View.VISIBLE);
+		case 2:
+			indicator = (ImageView) view.findViewById(R.id.image_view_indicator_2);
+			indicator.setVisibility(View.VISIBLE);
+		case 1:
+			indicator = (ImageView) view.findViewById(R.id.image_view_indicator_1);
+			indicator.setVisibility(View.VISIBLE);
+			break;
+
+		default:
+			break;
+		}
 
 		buttonYes = (Button) view.findViewById(R.id.button_yes);
 		buttonYes.setOnClickListener(this);
@@ -40,44 +93,20 @@ public class FragmentHabits extends Fragment implements OnClickListener {
 	public void onClick(View v) {
 
 		if(v.getId()==R.id.button_yes) {
-			switch (count) {
-			case 1:
-				setIndicatorAndQuestion(v, true, 1, "exercise");
-				break;
-			case 2:
-				setIndicatorAndQuestion(v, true, 2, "eat healthy");
-				break;
-			case 3:
-				setIndicatorAndQuestion(v, true, 3, "meditate");
-				break;
-			case 4:
-				setIndicatorAndQuestion(v, true, 4, "wake up early");
-				break;
-			default:
-				ImageView indicator5 = (ImageView) view.findViewById(R.id.image_view_indicator_5);
-				indicator5.setImageResource(R.drawable.indicator_positive);
-				break;
+			if(count != questionCount){
+				setIndicatorAndQuestion(v, true, count, habitQuestions.get(count));
+			} else {
+				ImageView indicator = (ImageView) view.findViewById(viewIndicators[count-1]);
+				indicator.setImageResource(R.drawable.indicator_positive);
 			}
 		} else if(v.getId()==R.id.button_no) {
-			switch (count) {
-			case 1:
-				setIndicatorAndQuestion(v, false, 1, "exercise");
-				break;
-			case 2:
-				setIndicatorAndQuestion(v, false, 2, "eat healthy");
-				break;
-			case 3:
-				setIndicatorAndQuestion(v, false, 3, "meditate");
-				break;
-			case 4:
-				setIndicatorAndQuestion(v, false, 4, "wake up early");
-				break;
-			default:
-				ImageView indicator5 = (ImageView) view.findViewById(R.id.image_view_indicator_5);
-				indicator5.setImageResource(R.drawable.indicator_negative);
-				break;
+			if(count != questionCount){
+				setIndicatorAndQuestion(v, false, count, habitQuestions.get(count));
+			} else {
+				ImageView indicator = (ImageView) view.findViewById(viewIndicators[count-1]);
+				indicator.setImageResource(R.drawable.indicator_negative);
 			}
-		} else if (v.getId() == R.id.test_button){
+		} else if (v.getId() == R.id.test_button){ // TODO delete this
 			Intent intetn = new Intent(getActivity(), EditHabits.class);
 			startActivity(intetn);
 		}
@@ -91,6 +120,23 @@ public class FragmentHabits extends Fragment implements OnClickListener {
 		TextView textViewHabit = (TextView) view.findViewById(R.id.text_view_did_i);
 		textViewHabit.setText(nextHabit);
 		count += 1;
+	}
+	
+	@Override
+	public void onDestroy() {
+	    super.onDestroy();
+	    if (databaseHelper != null) {
+	        OpenHelperManager.releaseHelper();
+	        databaseHelper = null;
+	    }
+	}
+
+	private DatabaseHelper getHelper() {
+	    if (databaseHelper == null) {
+	        databaseHelper =
+	            OpenHelperManager.getHelper(getActivity(), DatabaseHelper.class);
+	    }
+	    return databaseHelper;
 	}
 
 }
