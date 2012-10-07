6238f5c5e90632f75f63c8c9aca49246550f20d0
blaz <blaz@modrajagoda.si>
2012-10-07 12:12:11 +0200

    Chart now works, required some "special" code.

--- Did I/src/si/modrajagoda/didi/ViewPagerAdapterProgress.java
+++ Did I/src/si/modrajagoda/didi/ViewPagerAdapterProgress.java
@@ -165,8 +165,7 @@ public class ViewPagerAdapterProgress extends PagerAdapter {
 		int numberOfWeeks = (days.size()/7) + 1;
 		Log.d("WEEKNUM", "Number of weeks: " + numberOfWeeks);
 
-		double[] weeklyYesCount = new double[numberOfWeeks];
-		double[] anArrayOfSevens = new double[numberOfWeeks];
+		double[] weeklyYesCount = new double[numberOfWeeks + 6];
 
 		for(int i1 = 0; i1 < numberOfWeeks; i1++) {
 			int yesCount = 0;
@@ -183,29 +182,25 @@ public class ViewPagerAdapterProgress extends PagerAdapter {
 				}
 			}
 			weeklyYesCount[i1] = yesCount;
-			anArrayOfSevens[i1] = 7;
 		}
 
-		
+		for(int i3 = 0; i3 < 5; i3++) {
+			weeklyYesCount[numberOfWeeks + i3]=0;
+		}
 
 		List<double[]> values = new ArrayList<double[]>();
 		
-//		List<double[]> values = new ArrayList<double[]>();
-////		values.add(new double[] {7, 7, 7, 7, 7, 7, 7});
-//		values.add(new double[] {6, 7, 6, 6, 5, 1, 2});
-
-//		values.add(anArrayOfSevens);
 		values.add(weeklyYesCount);
 		
 		
 
-		Log.d("ARRAYS", "Arrays: " + weeklyYesCount.length + " " + anArrayOfSevens.length + " " + values.size());
+		Log.d("ARRAYS", "Arrays: " + weeklyYesCount.length + " " + values.size());
 		Log.d("calc", "calc: " + (numberOfWeeks + 0.5));
 		
 		LinearLayout layout = (LinearLayout) view.findViewById(R.id.chart);
 		XYMultipleSeriesRenderer renderer = buildBarRenderer(colors);
 		setChartSettings(renderer, "Week", 0.5,
-				1.5, 0, 7);
+				(numberOfWeeks + 0.5), 0, 7);
 		renderer.getSeriesRendererAt(0).setDisplayChartValues(false);
 		renderer.setPanEnabled(false, false);
 		mChartView = ChartFactory.getBarChartView(context, buildBarDataset(titles, values), renderer, Type.DEFAULT);
