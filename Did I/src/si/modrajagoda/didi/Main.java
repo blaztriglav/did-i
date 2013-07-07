95ee6ca1850236a33ba984d9ca7070e7b274646e
blaz <blaz@blaz-HP-Compaq-nc8430-RH471EA-AKN>
2013-07-07 19:52:17 +0200

    Single layout for progress and habits.

--- Did I/src/si/modrajagoda/didi/Main.java
+++ Did I/src/si/modrajagoda/didi/Main.java
@@ -1,48 +1,253 @@
 package si.modrajagoda.didi;
 
-import android.app.ActionBar;
-import android.app.FragmentTransaction;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+import si.modrajagoda.didi.database.DatabaseHelper;
+import si.modrajagoda.didi.database.Day;
+import si.modrajagoda.didi.database.Habit;
+
+import com.j256.ormlite.android.apptools.OpenHelperManager;
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.dao.ForeignCollection;
+
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.preference.PreferenceManager;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class Main extends FragmentActivity implements ActionBar.TabListener {
+public class Main extends FragmentActivity implements OnClickListener, OnPageChangeListener {
+	
+	private View view;
+	private int[] viewIndicators = {R.id.image_view_indicator_1, R.id.image_view_indicator_2, 
+			R.id.image_view_indicator_3, R.id.image_view_indicator_4, R.id.image_view_indicator_5};
 
-	private static final String STATE_SELECTED_NAVIGATION_ITEM = "selected_navigation_item";
-	private static final String TAB_TAG_HABITS = "habits";
-	private static final String TAB_TAG_PROGRESS = "progress";
+	private ArrayList<String> habitQuestions = null;
+	private int questionCount = 0;
+	private List<Habit> habits;
+	private DatabaseHelper databaseHelper = null;
+	private Dao<Habit, Integer> habitDao = null;
+	private Dao<Day, Integer> dayDao = null;
+
+	private Habit habit;
+	private ForeignCollection<Day> days;
+	private Day day;
+
+	private static SharedPreferences settings;
+	private Calendar date;
+	private int currentDay;
+	private int lastDayOfEntry;
+	private String LAST_DAY_OF_ENTRY = "LAST_DAY_OF_ENTRY";
+
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+	}
+	
+	@Override
+	public void onResume() {
+		super.onResume();
+		Log.d("IRAN", "RAN SO FAR AWAY");
+		loadUI();
+	}
 
-		// Set up the action bar.
-		final ActionBar actionBar = getActionBar();
-		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
 
-		// For each of the sections in the app, add a tab to the action bar.
-		actionBar.addTab(actionBar.newTab().setText(R.string.title_habits)
-				.setTag(TAB_TAG_HABITS).setTabListener(this));
-		actionBar.addTab(actionBar.newTab().setText(R.string.title_progress)
-				.setTag(TAB_TAG_PROGRESS).setTabListener(this));
+	private void loadUI() {
+		// Get all the habits from the database
+		databaseHelper = getHelper();
+		habitQuestions = new ArrayList<String>();
+		try {
+			habitDao = databaseHelper.getHabitDao();
+			habits = habitDao.queryForAll();
+			questionCount = habits.size();
+
+			for(Habit habit : habits){
+				habitQuestions.add(habit.getName());
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		// Check the date, add no's for unanswered days in between last check-in
+
+		settings = PreferenceManager.getDefaultSharedPreferences(this);
+
+		date = Calendar.getInstance();
+		currentDay = date.get(Calendar.DAY_OF_YEAR);
+
+		lastDayOfEntry = settings.getInt(LAST_DAY_OF_ENTRY, 0);
+
+		try {
+			dayDao = databaseHelper.getDayDao();
+
+			for(int i = 0; i < questionCount; i++) {
+
+				habit = habits.get(i);
+				days = habit.getDays();
+
+				if(lastDayOfEntry != 0) {
+
+					//If this is a newly added question, a day needs to be added either way
+					if(days.size()==0) {
+						day = new Day(habit, days.size()+1, false);
+						dayDao.create(day);
+					}
+					if(currentDay - lastDayOfEntry > 0) {
+
+						for (int i2 = 0; i2 < (currentDay-lastDayOfEntry); i2++) {
+							day = new Day(habit, days.size()+1, false);
+							dayDao.create(day);
+						}
+					}
+					
+					else if(currentDay - lastDayOfEntry < 0) {
+						day = new Day(habit, days.size()+1, false);
+						dayDao.create(day);
+					}
+					
+					settings.edit().putInt(LAST_DAY_OF_ENTRY, currentDay).commit();
+
+				}
+
+				else {
+					day = new Day(habit, days.size()+1, false);
+					dayDao.create(day);
+
+					settings.edit().putInt(LAST_DAY_OF_ENTRY, currentDay).commit();
+				}
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		// Dynamically display number of habits indicator
+		ImageView indicator;
+		switch (questionCount) {
+		case 5:
+			indicator = (ImageView) findViewById(R.id.image_view_indicator_5);
+			indicator.setVisibility(View.VISIBLE);
+		case 4:
+			indicator = (ImageView) findViewById(R.id.image_view_indicator_4);
+			indicator.setVisibility(View.VISIBLE);
+		case 3:
+			indicator = (ImageView) findViewById(R.id.image_view_indicator_3);
+			indicator.setVisibility(View.VISIBLE);
+		case 2:
+			indicator = (ImageView) findViewById(R.id.image_view_indicator_2);
+			indicator.setVisibility(View.VISIBLE);
+		case 1:
+			indicator = (ImageView) findViewById(R.id.image_view_indicator_1);
+			indicator.setVisibility(View.VISIBLE);
+			TextView textViewNoHabits = (TextView) findViewById(R.id.text_view_no_habits);
+			textViewNoHabits.setVisibility(View.GONE);
+			break;
+
+		default:
+			break;
+		}
+
+		//	loadDummyValues();
+
+		ViewPager pager = (ViewPager) findViewById(R.id.view_pager);
+		ViewPagerAdapterHabit adapter = new ViewPagerAdapterHabit(this, questionCount, habitQuestions, habits, databaseHelper);
+		pager.setAdapter(adapter);
+		pager.setOnPageChangeListener(this);
+		pager.setCurrentItem(0);
+
+	}
+
+	public static long daysBetween(Calendar lastDayOfEntry, Calendar currentDay) {  
+		Calendar date = (Calendar) lastDayOfEntry.clone();  
+		long daysBetween = 0;  
+		while (date.before(currentDay)) {  
+			date.add(Calendar.DAY_OF_MONTH, 1);  
+			daysBetween++;  
+		}  
+		return daysBetween;  
+	}  
+
+	private void loadDummyValues() {
+
+		try {
+			dayDao = databaseHelper.getDayDao();
+			habit = habits.get(0);
+
+			day = new Day(habit, days.size()+1, (Math.random() < 0.5));
+			dayDao.create(day);
+			days = habit.getDays();
+
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+
 	}
 
 	@Override
-	public void onRestoreInstanceState(Bundle savedInstanceState) {
-		if (savedInstanceState.containsKey(STATE_SELECTED_NAVIGATION_ITEM)) {
-			getActionBar().setSelectedNavigationItem(
-					savedInstanceState.getInt(STATE_SELECTED_NAVIGATION_ITEM));
+	public void onClick(View v) {
+
+	}
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		if (databaseHelper != null) {
+			OpenHelperManager.releaseHelper();
+			databaseHelper = null;
 		}
 	}
 
+	private DatabaseHelper getHelper() {
+		if (databaseHelper == null) {
+			databaseHelper =
+					OpenHelperManager.getHelper(this, DatabaseHelper.class);
+		}
+		return databaseHelper;
+	}
+
 	@Override
-	public void onSaveInstanceState(Bundle outState) {
-		outState.putInt(STATE_SELECTED_NAVIGATION_ITEM, getActionBar()
-				.getSelectedNavigationIndex());
+	public void onPageScrollStateChanged(int arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onPageScrolled(int arg0, float arg1, int arg2) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onPageSelected(int page) {
+
+		ImageView imageViewIndicator = (ImageView) findViewById(viewIndicators[page]);
+		imageViewIndicator.setImageResource(R.drawable.indicator_neutral_selected);
+
+		if(page > 0 ) {
+			imageViewIndicator = (ImageView) findViewById(viewIndicators[page-1]);
+			imageViewIndicator.setImageResource(R.drawable.indicator_neutral);
+		}
+		if(page < (viewIndicators.length)-1) {
+			imageViewIndicator = (ImageView) findViewById(viewIndicators[page+1]);
+			imageViewIndicator.setImageResource(R.drawable.indicator_neutral);
+		}
+
 	}
 
 	@Override
@@ -60,38 +265,6 @@ public class Main extends FragmentActivity implements ActionBar.TabListener {
 			break;
 		}
 		return super.onOptionsItemSelected(item);
-	}
-	
-	@Override
-	public void onTabUnselected(ActionBar.Tab tab,
-			FragmentTransaction fragmentTransaction) {
-	}
-
-	@Override
-	public void onTabSelected(ActionBar.Tab tab,
-			FragmentTransaction fragmentTransaction) {
-		// When the given tab is selected, show the tab contents in the
-		// container
-		if (tab.getTag() == TAB_TAG_HABITS) {
-			Fragment fragment = new FragmentHabits();
-			Bundle args = new Bundle();
-			fragment.setArguments(args);
-			getSupportFragmentManager().beginTransaction()
-					.replace(R.id.container, fragment).commit();
-		}
-		if (tab.getTag() == TAB_TAG_PROGRESS) {
-			Fragment fragment = new FragmentProgress();
-			Bundle args = new Bundle();
-			fragment.setArguments(args);
-			getSupportFragmentManager().beginTransaction()
-					.replace(R.id.container, fragment).commit();
-		}
-
-	}
-
-	@Override
-	public void onTabReselected(ActionBar.Tab tab,
-			FragmentTransaction fragmentTransaction) {
 	}
 	
 }
--- Did I/src/si/modrajagoda/didi/ViewPagerAdapterHabit.java
+++ Did I/src/si/modrajagoda/didi/ViewPagerAdapterHabit.java
@@ -4,6 +4,14 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.achartengine.ChartFactory;
+import org.achartengine.GraphicalView;
+import org.achartengine.chart.BarChart.Type;
+import org.achartengine.model.CategorySeries;
+import org.achartengine.model.XYMultipleSeriesDataset;
+import org.achartengine.renderer.SimpleSeriesRenderer;
+import org.achartengine.renderer.XYMultipleSeriesRenderer;
+
 import com.j256.ormlite.dao.CloseableIterable;
 import com.j256.ormlite.dao.CloseableIterator;
 import com.j256.ormlite.dao.Dao;
@@ -16,6 +24,7 @@ import si.modrajagoda.didi.database.Day;
 import si.modrajagoda.didi.database.Habit;
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Color;
 import android.os.Parcelable;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
@@ -24,12 +33,16 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.webkit.WebView.FindListener;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class ViewPagerAdapterHabit extends PagerAdapter implements OnClickListener {
 
+	private GraphicalView mChartView;
+	
 	private final Context context;
 	private final int questionCount;
 	private final ArrayList<String> habitQuestions;
@@ -73,6 +86,8 @@ public class ViewPagerAdapterHabit extends PagerAdapter implements OnClickListen
 		switch (position) {
 		case 0:
 			View v = inflater.inflate(viewPagerPageHabit, null);
+			
+			v.setTag(0);
 			loadQuestion(v, 0);
 
 			((ViewPager) pager).addView(v, 0);
@@ -80,6 +95,7 @@ public class ViewPagerAdapterHabit extends PagerAdapter implements OnClickListen
 		case 1:
 
 			v = inflater.inflate(viewPagerPageHabit, null);
+			v.setTag(1);
 			loadQuestion(v, 1);
 
 
@@ -87,6 +103,7 @@ public class ViewPagerAdapterHabit extends PagerAdapter implements OnClickListen
 			return v;
 		case 2:
 			v = inflater.inflate(viewPagerPageHabit, null);
+			v.setTag(2);
 			loadQuestion(v, 2);
 
 
@@ -94,6 +111,7 @@ public class ViewPagerAdapterHabit extends PagerAdapter implements OnClickListen
 			return v;
 		case 3:
 			v = inflater.inflate(viewPagerPageHabit, null);
+			v.setTag(3);
 			loadQuestion(v, 3);
 
 
@@ -101,6 +119,7 @@ public class ViewPagerAdapterHabit extends PagerAdapter implements OnClickListen
 			return v;
 		case 4:
 			v = inflater.inflate(viewPagerPageHabit, null);
+			v.setTag(4);
 			loadQuestion(v, 4);
 
 			((ViewPager) pager).addView(v, 0);
@@ -132,13 +151,151 @@ public class ViewPagerAdapterHabit extends PagerAdapter implements OnClickListen
 		buttonNo = (Button) view.findViewById(R.id.button_no);
 		buttonNo.setOnClickListener(this);
 		buttonNo.setTag("Q"+question);
+		
+		loadChart(view, question);
 
 	}
 
+	private void loadChart(View view, int question) {
+		int[] colors = new int[] {context.getResources().getColor(R.color.positive)};
+		String[] titles = new String[] { "Yes" };
+
+		//Get all the days entered so far for this habit
+		habit = habits.get(question);
+		days = habit.getDays();
+
+		//Fill an array with the answers for those days (true, false) by iterating through all the days
+		double[] answersArray = new double[days.size()];
+		int count = 0;
+		CloseableIterator<Day> daysIterator = days.closeableIterator();
+		while(daysIterator.hasNext()) {
+			day = daysIterator.next();
+			
+			if(day.getDayAnswer()==true) {
+				answersArray[count] = 1;
+			}
+			else if(day.getDayAnswer()==false) {
+				answersArray[count] = 0;
+			}
+		
+			count = count + 1;
+		}
+		
+		try {
+			daysIterator.close();
+		} catch (SQLException e1) {
+			e1.printStackTrace();
+		}
+
+		//Calculate the number of weeks entered for the habit so far (the chart will display yes/no's by week)
+		//The days()size - 1 is so we get the proper week count (7 days comes out to 2 weeks without this). Good code TM.
+
+		int numberOfWeeks = ((days.size()- 1)/7)+ 1;
+
+		//Make array with the size of the number of weeks + 6 
+		//The +6 is to make the chart render properly, for some reason it needs those dummy values. Good code TM.
+		
+		double[] weeklyYesCount = new double[numberOfWeeks + 6];
+
+		//Get the amount of yes's per week, add it to the array
+		for(int i1 = 0; i1 < numberOfWeeks; i1++) {
+			int yesCount = 0;
+
+			for (int i2 = 0; i2 < 7; i2++) {
+				try {
+					
+					if(answersArray[i2 + (7*i1)]==1) {
+						yesCount = yesCount + 1;
+					}
+				} catch ( ArrayIndexOutOfBoundsException e ) {
+				}
+			}
+			weeklyYesCount[i1] = yesCount;
+			
+		}
+
+		List<double[]> values = new ArrayList<double[]>();
+		values.add(weeklyYesCount);
+		
+		//Draw the chart. Every week is displayed as a separate column, for a maximum of 7.
+		//If there is data for more than 7 weeks, the last 7 are presented.
+		//This felt like a nice amount that gives you perspective on past performance
+		//and still keeps every week neatly visible.
+		
+		int numberOfWeeksOverSeven = 0;
+		if (numberOfWeeks>7) {
+			numberOfWeeksOverSeven = numberOfWeeks-7;
+		}
+		
+		LinearLayout layout = (LinearLayout) view.findViewById(R.id.chart);
+		XYMultipleSeriesRenderer renderer = buildBarRenderer(colors);
+		setChartSettings(renderer, 
+				"Week", 
+				(numberOfWeeksOverSeven + 0.5),
+				(numberOfWeeks + 0.5), 
+				0, 
+				7);
+		
+		mChartView = ChartFactory.getBarChartView(context, 
+				buildBarDataset(titles, values), 
+				renderer, 
+				Type.DEFAULT);
+		
+		mChartView.setTag("C" + question);
+		
+		layout.addView(mChartView);
+
+	}
+
+	protected void setChartSettings(XYMultipleSeriesRenderer renderer, String xTitle,
+			double xMin, double xMax, double yMin, double yMax) {
+		renderer.setXTitle(xTitle);
+		renderer.setXAxisMin(xMin);
+		renderer.setXAxisMax(xMax);
+		renderer.setYAxisMin(yMin);
+		renderer.setYAxisMax(yMax);
+		renderer.setShowLegend(false);
+		renderer.setShowAxes(false);
+		renderer.setShowLabels(false);
+		renderer.getSeriesRendererAt(0).setDisplayChartValues(false);
+		renderer.setPanEnabled(false, false);
+		renderer.setZoomButtonsVisible(false);
+		renderer.setMarginsColor(Color.argb(0, 1, 1, 1));
+		renderer.setBackgroundColor(context.getResources().getColor(R.color.negative));
+		renderer.setApplyBackgroundColor(true);
+		renderer.setMargins(new int[] {0,0,0,0});
+	}
+
+	protected XYMultipleSeriesDataset buildBarDataset(String[] titles, List<double[]> values) {
+		XYMultipleSeriesDataset dataset = new XYMultipleSeriesDataset();
+		int length = titles.length;
+		for (int i = 0; i < length; i++) {
+			CategorySeries series = new CategorySeries(titles[i]);
+			double[] v = values.get(i);
+			int seriesLength = v.length;
+			for (int k = 0; k < seriesLength; k++) {
+				series.add(v[k]);
+			}
+			dataset.addSeries(series.toXYSeries());
+		}
+		return dataset;
+	}
+
+	protected XYMultipleSeriesRenderer buildBarRenderer(int[] colors) {
+		XYMultipleSeriesRenderer renderer = new XYMultipleSeriesRenderer();
+		int length = colors.length;
+		for (int i = 0; i < length; i++) {
+			SimpleSeriesRenderer r = new SimpleSeriesRenderer();
+			r.setColor(colors[i]);
+			renderer.addSeriesRenderer(r);
+		}
+		return renderer;
+	}
+	
 	@Override
 	public void destroyItem( View pager, int position, Object view )
 	{
-		((ViewPager)pager).removeView( (RelativeLayout)view );
+		((ViewPager)pager).removeView( (LinearLayout)view );
 	}
 
 	@Override
@@ -167,28 +324,36 @@ public class ViewPagerAdapterHabit extends PagerAdapter implements OnClickListen
 
 		ViewPager viewPager = (ViewPager) ((Activity) context).findViewById(R.id.view_pager);
 
-
 		//Question 1
 		if(v.getId()==R.id.button_yes) {
 			if(v.getTag().equals("Q0")) {
-				
 				answerQuestion(0, true);
+				reloadChart(0, viewPager);
+				
 				viewPager.setCurrentItem(1); 
 			}
 			else if(v.getTag().equals("Q1")) {
 				answerQuestion(1, true);
+				reloadChart(1, viewPager);
+
 				viewPager.setCurrentItem(2); 
 			}
 			else if(v.getTag().equals("Q2")) {
 				answerQuestion(2, true);
+				reloadChart(2, viewPager);
+				
 				viewPager.setCurrentItem(3); 
 			}
 			else if(v.getTag().equals("Q3")) {
 				answerQuestion(3, true);
+				reloadChart(3, viewPager);
+				
 				viewPager.setCurrentItem(4); 
 			}
 			else if(v.getTag().equals("Q4")) {
 				answerQuestion(4, true);
+				reloadChart(4, viewPager);
+				
 				viewPager.setCurrentItem(4); 
 			}
 
@@ -197,26 +362,46 @@ public class ViewPagerAdapterHabit extends PagerAdapter implements OnClickListen
 		else if(v.getId()==R.id.button_no) {
 			if(v.getTag().equals("Q0")) {
 				answerQuestion(0, false);
+				reloadChart(0, viewPager);
+				
 				viewPager.setCurrentItem(1); 
 			}
 			else if(v.getTag().equals("Q1")) {
 				answerQuestion(1, false);
+				reloadChart(1, viewPager);
+				
 				viewPager.setCurrentItem(2); 
 			}
 			else if(v.getTag().equals("Q2")) {
 				answerQuestion(2, false);
+				reloadChart(2, viewPager);
+				
 				viewPager.setCurrentItem(3); 
 			}
 			else if(v.getTag().equals("Q3")) {
 				answerQuestion(3, false);
+				reloadChart(3, viewPager);
+				
 				viewPager.setCurrentItem(4); 
 			}
 			else if(v.getTag().equals("Q4")) {
 				answerQuestion(4, false);
+				reloadChart(4, viewPager);
+				
 				viewPager.setCurrentItem(4); 
 			}
 		}
 
+
+	}
+
+	private void reloadChart(int i, ViewPager vp) {
+
+		View questionView = vp.findViewWithTag(i);
+		LinearLayout layoutL = (LinearLayout) questionView.findViewById(R.id.chart);
+		layoutL.removeAllViews();
+		loadQuestion(questionView, i);
+		
 	}
 
 	private void answerQuestion(int question, boolean answer) {
